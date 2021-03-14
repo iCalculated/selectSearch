49f60b60166ece9b3fce168308a57c57979bc969
iCalculated <fufa0001@gmail.com>
2021-03-14 13:41:03 -0500

    added search trigger listener

--- background.js
+++ background.js
@@ -4,3 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color }); 
   console.log('Default background color set to %cgreen', `color: ${color}`); 
 });
+
+chrome.commands.onCommand.addListener(function(command) {
+    console.log('Command:', command);
+});
