1912d6da549153e7dfc533cc396d4c38371f1c34
iCalculated <fufa0001@gmail.com>
2021-03-14 14:31:06 -0500

    omnibox investigation

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+function navigate(url) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {url: url})
+  });
+}
 let color = '#3aa757'; 
 
 chrome.runtime.onInstalled.addListener(() => { 
@@ -6,5 +11,11 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.commands.onCommand.addListener(function(command) {
-    console.log('Command:', command);
+  if (command == "trigger_search") {
+    console.log(window);
+  }
+});
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  navigate("https://www.google.com");
 });
