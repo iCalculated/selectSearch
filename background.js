f182ea1c953c63d0e8f33f8f061835a1f6c430c8
iCalculated <fufa0001@gmail.com>
2021-03-14 21:19:46 -0500

    further cleanup

--- background.js
+++ background.js
@@ -1,18 +1,4 @@
-function navigate(url) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: url})
-  });
-}
-
-let color = '#3aa757'; 
-
-chrome.runtime.onInstalled.addListener(() => { 
-  chrome.storage.sync.set({ color }); 
-  //console.log('Default background color set to %cgreen', `color: ${color}`); 
-});
-
 chrome.commands.onCommand.addListener(function(command) {
-
   if (command == "trigger_search") {  
     chrome.tabs.query({active: true, currentWindow:true}, function(tabs){
       chrome.scripting.executeScript(
